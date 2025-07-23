949f639839ee63c1ae80a0255575e5fed282d87c
Henok-et <henok.kinde-ug@aau.edu.et>
2025-07-23 11:21:27 +0300

    feat: add DebugRouteLogger for route navigation logging

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	useLocation,
+} from "react-router-dom";
 import Topics from "./components/Topics";
 import QuestionnaireForm from "./components/QuestionnaireForm";
 import ConfirmationPage from "./components/ConfirmationPage";
 import QRCodePage from "./components/QRCodePage";
 import "./App.css";
 
+function DebugRouteLogger() {
+	const location = useLocation();
+	React.useEffect(() => {
+		console.log("[Router] Navigated to:", location.pathname);
+	}, [location]);
+	return null;
+}
+
 function App() {
+	React.useEffect(() => {
+		console.log("[App] App component mounted");
+	}, []);
 	return (
 		<Router>
+			<DebugRouteLogger />
 			<div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-4">
 				<Routes>
 					<Route path="/qr" element={<QRCodePage />} />
